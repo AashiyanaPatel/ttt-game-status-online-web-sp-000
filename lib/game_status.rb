e876d26a4774628bf2740b59ff7edbe06f3c7339
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-02 17:19:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,10 +30,6 @@ def valid_move?(board, index)
   index.between?(0,8)  && !position_taken?(board, index)
 end
 
-board.count do
-  cell != ""
-  end
-
 def current_player(board)
   if turn_count(board).odd?
     "O"
